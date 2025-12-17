696ee5c776f8898d5ea392663baa2346604d1d74
tareklaaouane <tlaaouane@gmail.com>
2025-12-17 01:01:57 +0100

    add

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Calculator A = new Calculator();
         System.out.println(A.add(1, 2));
-
+        System.out.println(A.add(1, 2));
     }
 }
